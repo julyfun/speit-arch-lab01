348baa7e9f4ce55c7247e1bff84fc3ed5108e852
4070s wsl julyfun <julyfun.collect@outlook.com>
2024-10-16 16:25:29 +0800

    M .gitignore | M hello.c | M ll_cycle.c | M no_segfault_ex.c | ?? hello | ?? name.txt

--- hello.c
+++ hello.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+void func() {
+    int l = 1;
+    int c = l + 2;
+}
+
 int main(int argc, char *argv[]) {
     int i;
     int count = 0;
     int *p = &count;
+
+    func();
 
     for (i = 0; i < 10; i++) {
         (*p)++; // Do you understand this line of code and all the other permutations of the operators? ;)
--- ll_cycle.c
+++ ll_cycle.c
@@ -3,5 +3,14 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node* u = head;
+    node* v = head;
+    while (v != NULL && v->next != NULL) {
+        v = v->next->next;
+        u = u->next;
+        if (u == v) {
+            return 1;
+        }
+    }
     return 0;
 }
